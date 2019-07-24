1caedbfc73f62d6bf13fd6697b729025503668ef
leinalou <leinagries@gmail.com>
2019-07-24 13:29:37 -0600

    beginning basics, getting git up to day 3

---
+++ day3/testing_values.py
@@ -0,0 +1,41 @@
+def main():
+    import math
+import matplotlib.pyplot as plt
+dt = 0.001
+N0B = 2
+N0W = 2
+Prey_Growth_Rate = 1
+Prey_Side_Interaction_Rate = 0.3
+Predator_Side_Interaction_Rate = 0.5
+Predator_Death_Rate = 0.8
+NGen = 90000
+NB_Previous = N0B
+NW_Previous = N0W
+
+for i in range(NGen):
+    dNB =(Prey_Growth_Rate * NB_Previous * dt) - (Prey_Side_Interaction_Rate * NB_Previous * NW_Previous * dt)
+    NB_New = NB_Previous + dNB
+    #NB_Previous = NB_New
+    dNW = (Predator_Side_Interaction_Rate * NB_Previous * NW_Previous* dt) - (Predator_Death_Rate * NW_Previous * dt)
+    NW_New = NW_Previous + dNW
+  # NB_Previous = NB_New
+  # NW_Previous = NW_New
+  #rint(NB_New,NW_New)
+    if NB_Previous <= 0:
+        NB_New = 0
+    else:
+        NB_Previous = NB_New
+    if NW_Previous <= 0:
+        NW_New = 0
+    else:
+        NW_Previous = NW_New    
+    print(NB_New,NW_New)
+    if NW_New  == 0 and NB_New == 0:
+        break
+
+#plt.plot(NB_New,NW_New)
+#plt.show()
+        
+   #print(NW_New)
+main()
+
