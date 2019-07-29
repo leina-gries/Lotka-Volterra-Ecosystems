ed915898807d433a53a5a72a1e7d5005eb240c96
leinalou <leinagries@gmail.com>
2019-07-29 10:29:05 -0600

    should be adding the basic one species model

--- pred_prey_int.py
+++ pred_prey_int.py
@@ -1,15 +1,17 @@
-#! /usr/bin/env python3
-
-
 def main():
-    
     import math
-A = input("starting population?")
-int_A = int(A)
-print(int_A)
-
-for i in range(20):
-    y = pow(int_A, i)
-    print(i, ';', y)
-
-main()
+#terms and coefficients#
+dt = 0.001
+#^change in time#
+N0 = 2
+#^original population#
+Growth_Rate = 0.20
+NGen = 50000
+N_Previous = N0
+for i in range(NGen):
+    dN = Growth_Rate * N_Previous * dt
+    N_New = N_Previous + dN
+    print(N_New)
+    N_Previous = N_New
+#successful graphing of a single species exp growth model##
+##editing editing wiorkkkkkk#####
